1f422e331c5e74fdc0f1fb37dcdcf7530c7c5067
Philip Graham <philip.robert.graham@gmail.com>
2014-08-25 14:33:10 +0000

    [minor] CS fixes

--- zpt/pct/TemplateValues.php
+++ zpt/pct/TemplateValues.php
@@ -1,20 +1,15 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2012, Philip Graham
+/*
+ * Copyright (c) 2014, Philip Graham
  * All rights reserved.
  *
- * This file is part of php-code-templates and is licensed by the Copyright
- * holder under the 3-clause BSD License.  The full text of the license can be
- * found in the LICENSE.txt file included in the root directory of this
- * distribution or at the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * This file is part of php-code-templates. For the full copyright and license
+ * information please view the LICENSE file that was distributed with this
+ * source code.
  */
 namespace zpt\pct;
 
-use \ArrayAccess;
+use ArrayAccess;
 
 /**
  * This class encapsulates a set of values for template substitution.
@@ -24,60 +19,54 @@ use \ArrayAccess;
 class TemplateValues implements ArrayAccess
 {
 
-    const AR_RE = '/([[:alnum:]_-]+)\[([[:alnum:]_-]+)\]/';
+	const AR_RE = '/([[:alnum:]_-]+)\[([[:alnum:]_-]+)\]/';
 
-    private $values;
+	private $values;
 
-    /**
-     * Create a new TemplateValues object encapsulating the given values.
-     *
-     * @param array $values
-     */
-    public function __construct(array $values)
-    {
-        $this->values = $values;
-    }
-    
-    public function getValue($name)
-    {
-        if (preg_match(self::AR_RE, $name, $matches)) {
-            $name = $matches[1];
-            $idx = $matches[2];
+	/**
+	 * Create a new TemplateValues object encapsulating the given values.
+	 *
+	 * @param array $values
+	 */
+	public function __construct(array $values) {
+		$this->values = $values;
+	}
 
-            if (isset($this->values[$name][$idx])) {
-                return $this->values[$name][$idx];
-            }
-        } else {
-            if (isset($this->values[$name])) {
-                return $this->values[$name];
-            }
-        }
-        return null;
-    }
+	public function getValue($name) {
+		if (preg_match(self::AR_RE, $name, $matches)) {
+			$name = $matches[1];
+			$idx = $matches[2];
 
-    public function offsetExists($offset)
-    {
-        return isset($this->values[$offset]);
-    }
+			if (isset($this->values[$name][$idx])) {
+				return $this->values[$name][$idx];
+			}
+		} else {
+			if (isset($this->values[$name])) {
+				return $this->values[$name];
+			}
+		}
+		return null;
+	}
 
-    public function offsetGet($offset)
-    {
-        return isset($this->values[$offset])
-            ? $this->values[$offset]
-            : null;
-    }
+	public function offsetExists($offset) {
+		return isset($this->values[$offset]);
+	}
 
-    public function offsetSet($offset, $value)
-    {
-        if ($offset === null) {
-            $this->values[] = $value;
-        } else {
-            $this->values[$offset] = $value;
-        }
-    }
+	public function offsetGet($offset) {
+		return isset($this->values[$offset])
+			? $this->values[$offset]
+			: null;
+	}
 
-    public function offsetUnset($offset)
-    {
-        unset($this->values[$offset]);
-    }
+	public function offsetSet($offset, $value) {
+		if ($offset === null) {
+			$this->values[] = $value;
+		} else {
+			$this->values[$offset] = $value;
+		}
+	}
+
+	public function offsetUnset($offset) {
+		unset($this->values[$offset]);
+	}
 }
