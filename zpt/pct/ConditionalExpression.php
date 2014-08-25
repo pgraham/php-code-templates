befa711a6e2aa42b3379d8d22d6bcfbde6cdb757
Philip Graham <philip.robert.graham@gmail.com>
2014-08-25 16:16:57 +0000

    [minor] CS Fixes

--- zpt/pct/ConditionalExpression.php
+++ zpt/pct/ConditionalExpression.php
@@ -1,234 +1,230 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2012, Philip Graham
+/*
+ * Copyright (c) 2014, Philip Graham
  * All rights reserved.
  *
- * This file is part of php-code-templates and is licensed by the Copyright
- * holder under the 3-clause BSD License.  The full text of the license can be
- * found in the LICENSE.txt file included in the root directory of this
- * distribution or at the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * This file is part of php-code-templates. For the full copyright and license
+ * information please view the LICENSE file that was distributed with this
+ * source code.
  */
 namespace zpt\pct;
 
-use \Exception;
+use Exception;
 
 /**
  * This class encapsulates expression evaluation for a conditional clause of a
  * {@link ConditionalBlock}.
  *
- *   NOTE: There is a known issue that using any of the operators, or the words
- *         'and' or 'or' as comparison values will not behave as expected.
+ *	 NOTE: There is a known issue that using any of the operators, or the words
+ *				 'and' or 'or' as comparison values will not behave as expected.
  *
  * @author Philip Graham <philip@zeptech.ca>
  */
-class ConditionalExpression {
+class ConditionalExpression
+{
 
-  /* Whether or not the class has been statically constructed. */
-  private static $_initialized = false;
+	/* Whether or not the class has been statically constructed. */
+	private static $initialized = false;
 
-  /*
-   * Non-capturing regular expression for all supported operators.  Any
-   * strings matched by this regexp must have a matching entry in opEvaluators.
-   */
-  private static $_ops = '(?:=|>|>=|<|<=|!=|ISSET|ISNOTSET)';
+	/*
+	 * Non-capturing regular expression for all supported operators. Any
+	 * strings matched by this regexp must have a matching entry in opEvaluators.
+	 */
+	private static $ops = '(?:=|>|>=|<|<=|!=|ISSET|ISNOTSET)';
 
-  private static $_opEvaluators;
+	private static $opEvaluators;
 
-  /*
-   * Static constructor.  Will happen the first time an instance of this class
-   * is created.
-   */
-  private static function _initialize() {
-    self::$_initialized = true;
+	/*
+	 * Static constructor.	Will happen the first time an instance of this class
+	 * is created.
+	 */
+	private static function initialize() {
+		self::$initialized = true;
 
-    self::$_opEvaluators = array(
-      // '=' Evaluator
-      '=' => function ($a, $b) {
-        return $a === $b;
-      },
+		self::$opEvaluators = array(
+			// '=' Evaluator
+			'=' => function ($a, $b) {
+				return $a === $b;
+			},
 
-      // '>' Evaluator
-      '>' => function ($a, $b) {
-        return $a > $b;
-      },
+			// '>' Evaluator
+			'>' => function ($a, $b) {
+				return $a > $b;
+			},
 
-      // '>=' Evaluator
-      '>=' => function ($a, $b) {
-        return $a >= $b;
-      },
+			// '>=' Evaluator
+			'>=' => function ($a, $b) {
+				return $a >= $b;
+			},
 
-      // '<' Evaluator
-      '<' => function ($a, $b) {
-        return $a < $b;
-      },
+			// '<' Evaluator
+			'<' => function ($a, $b) {
+				return $a < $b;
+			},
 
-      // '<=' Evaluator
-      '<=' => function ($a, $b) {
-        return $a <= $b;
-      },
+			// '<=' Evaluator
+			'<=' => function ($a, $b) {
+				return $a <= $b;
+			},
 
-      // '!=' Evaluator
-      '!=' => function ($a, $b) {
-        return $a !== $b;
-      },
+			// '!=' Evaluator
+			'!=' => function ($a, $b) {
+				return $a !== $b;
+			},
 
-      'ISSET' => function ($a, $b) {
-        return $a !== null;
-      },
+			'ISSET' => function ($a, $b) {
+				return $a !== null;
+			},
 
-      'ISNOTSET' => function ($a, $b) {
-        return $a === null;
-      }
-    );
-  }
+			'ISNOTSET' => function ($a, $b) {
+				return $a === null;
+			}
+		);
+	}
 
-  /**
-   * Static function to determine if the specified string is a valid operator.
-   *
-   * @param string $op
-   * @return boolean
-   */
-  public static function isValidOperator($op) {
-    return preg_match('/' . self::$_ops . '/', trim($op));
-  }
+	/**
+	 * Static function to determine if the specified string is a valid operator.
+	 *
+	 * @param string $op
+	 * @return boolean
+	 */
+	public static function isValidOperator($op) {
+		return preg_match('/' . self::$ops . '/', trim($op));
+	}
 
-  /*
-   * ===========================================================================
-   * Instance
-   * ===========================================================================
-   */
+	/*
+	 * ===========================================================================
+	 * Instance
+	 * ===========================================================================
+	 */
 
-  private $_conditions;
+	private $_conditions;
 
-  /**
-   * Create a new ConditionalExpression.
-   *
-   * @param string $expression Unparsed expression string.
-   */
-  public function __construct($expression) {
-    if (!self::$_initialized) {
-      self::_initialize();
-    }
+	/**
+	 * Create a new ConditionalExpression.
+	 *
+	 * @param string $expression Unparsed expression string.
+	 */
+	public function __construct($expression) {
+		if (!self::$initialized) {
+			self::initialize();
+		}
 
-    $ops = self::$_ops;
-    $varRe = '[[:alnum:]_-]+(?:\[[[:alnum:]_-]+\])?';
-    $logicRe = "\s*($varRe\s*(?:$ops\s*$varRe)?)\s+(or|and)\s+(.*)\s*";
+		$ops = self::$ops;
+		$varRe = '[[:alnum:]_-]+(?:\[[[:alnum:]_-]+\])?';
+		$logicRe = "\s*($varRe\s*(?:$ops\s*$varRe)?)\s+(or|and)\s+(.*)\s*";
 
-    $curGroup = array();
+		$curGroup = array();
 
-    $exp = $expression;
-    while ($exp !== null) {
-      $matches = array();
-      if (preg_match("/$logicRe/", $exp, $matches)) {
-        $comp = trim($matches[1]);
-        $logic = trim($matches[2]);
-        $exp = trim($matches[3]);
+		$exp = $expression;
+		while ($exp !== null) {
+			$matches = array();
+			if (preg_match("/$logicRe/", $exp, $matches)) {
+				$comp = trim($matches[1]);
+				$logic = trim($matches[2]);
+				$exp = trim($matches[3]);
 
-        $cond = $this->_buildCondition($comp);
+				$cond = $this->buildCondition($comp);
 
-      } else {
-        $cond = $this->_buildCondition($exp);
-        $logic = null;
-        $exp = null;
-      }
+			} else {
+				$cond = $this->buildCondition($exp);
+				$logic = null;
+				$exp = null;
+			}
 
-      $curGroup[] = $cond;
-      if ($logic === 'and') {
-        $this->_conditions[] = $curGroup;
-        $curGroup = array();
-      }
-    }
-    $this->_conditions[] = $curGroup;
-  }
+			$curGroup[] = $cond;
+			if ($logic === 'and') {
+				$this->_conditions[] = $curGroup;
+				$curGroup = array();
+			}
+		}
+		$this->_conditions[] = $curGroup;
+	}
 
-  public function __toString() {
-    $ands = array();
-    foreach ($this->_conditions as $conditionGroup) {
-      $ors = array();
-      foreach ($conditionGroup as $condition) {
-        $ors[] = "$condition[name] $condition[op] $condition[val]";
-      }
-      $ands[] = implode(' OR ', $ors);
-    }
-    return implode(' AND ', $ands);
-  }
+	public function __toString() {
+		$ands = array();
+		foreach ($this->_conditions as $conditionGroup) {
+			$ors = array();
+			foreach ($conditionGroup as $condition) {
+				$ors[] = "$condition[name] $condition[op] $condition[val]";
+			}
+			$ands[] = implode(' OR ', $ors);
+		}
+		return implode(' AND ', $ands);
+	}
 
-  /**
-   * Determines whether or not the encapsulated expression evaluates to true for
-   * the given set of values.
-   *
-   * @param Array $values Set of substitution values.
-   * @return boolean
-   */
-  public function isSatisfiedBy($values) {
-    if (is_array($values)) {
-      $values = new TemplateValues($values);
-    }
+	/**
+	 * Determines whether or not the encapsulated expression evaluates to true for
+	 * the given set of values.
+	 *
+	 * @param Array $values Set of substitution values.
+	 * @return boolean
+	 */
+	public function isSatisfiedBy($values) {
+		if (is_array($values)) {
+			$values = new TemplateValues($values);
+		}
 
-    if (!($values instanceof TemplateValues)) {
-      throw new Exception("Given values must be either an array or a " .
-        "TemplateValues instance.");
-    }
+		if (!($values instanceof TemplateValues)) {
+			throw new Exception("Given values must be either an array or a " .
+				"TemplateValues instance.");
+		}
 
-    foreach ($this->_conditions as $group) {
-      $groupSatisfied = false;
-      foreach ($group as $cond) {
-        $val = $values->getValue($cond['name']);
+		foreach ($this->_conditions as $group) {
+			$groupSatisfied = false;
+			foreach ($group as $cond) {
+				$val = $values->getValue($cond['name']);
 
-        if ($cond['val'] === null) {
-          // Note the use of weak equality operator here.
-          if ($val == true) {
-            $groupSatisfied = true;
-            break;
-          }
-        } else {
-          $fn = self::$_opEvaluators[$cond['op']];
-          if ($fn($val, $cond['val'])) {
-            $groupSatisfied = true;
-            return true;
-          }
-        }
-      }
+				if ($cond['val'] === null) {
+					// Note the use of weak equality operator here.
+					if ($val == true) {
+						$groupSatisfied = true;
+						break;
+					}
+				} else {
+					$fn = self::$opEvaluators[$cond['op']];
+					if ($fn($val, $cond['val'])) {
+						$groupSatisfied = true;
+						return true;
+					}
+				}
+			}
 
-      if (!$groupSatisfied) {
-        return false;
-      }
-    }
+			if (!$groupSatisfied) {
+				return false;
+			}
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  private function _buildCondition($exp) {
-    $ops = self::$_ops;
+	private function buildCondition($exp) {
+		$ops = self::$ops;
 
-    $matches = array();
-    if (preg_match("/\s*(.+)\s*($ops)\s*(.*)\s*/", $exp, $matches)) {
-      $name = trim($matches[1]);
-      $op = trim($matches[2]);
-      $val = trim($matches[3]);
-    } else {
-      $name = $exp;
-      $op = null;
-      $val = null;
-    }
+		$matches = array();
+		if (preg_match("/\s*(.+)\s*($ops)\s*(.*)\s*/", $exp, $matches)) {
+			$name = trim($matches[1]);
+			$op = trim($matches[2]);
+			$val = trim($matches[3]);
+		} else {
+			$name = $exp;
+			$op = null;
+			$val = null;
+		}
 
-    if (is_numeric($val)) {
-      $val = (float) $val;
+		if (is_numeric($val)) {
+			$val = (float) $val;
 
-      if ((int) $val == $val) {
-        $val = (int) $val;
-      }
-    }
+			if ((int) $val == $val) {
+				$val = (int) $val;
+			}
+		}
 
-    return array(
-      'name' => $name,
-      'op'   => $op,
-      'val'  => $val
-    );
-  }
+		return array(
+			'name' => $name,
+			'op'	 => $op,
+			'val'  => $val
+		);
+	}
 
 }
