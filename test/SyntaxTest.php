f8498275a74f72424b9435e458badcf24e1d50f1
Philip Graham <philip.robert.graham@gmail.com>
2014-08-28 13:39:02 -0400

    [minor] CS Fixes

--- test/SyntaxTest.php
+++ test/SyntaxTest.php
@@ -1,20 +1,15 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2012, Philip Graham
+/*
+ * Copyright (c) 2012 - 2014, Philip Graham
  * All rights reserved.
  *
- * This file is part of php-code-templates and is licensed by the Copyright
- * holder under the 3-clause BSD License.  The full text of the license can be
- * found in the LICENSE.txt file included in the root directory of this
- * distribution or at the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * This file is part of php-code-templates. For the full copyright and license
+ * information please view the LICENSE file that was distributed with this
+ * source code.
  */
 namespace zpt\pct;
 
-use \PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit_Framework_TestCase as TestCase;
 
 require_once __DIR__ . '/test-common.php';
 
@@ -23,27 +18,28 @@ require_once __DIR__ . '/test-common.php';
  *
  * @author Philip Graham <philip@zeptech.ca>
  */
-class SyntaxText extends TestCase {
+class SyntaxText extends TestCase
+{
 
-  /*
-   * ===========================================================================
-   * Erroneous template fragments.
-   * ===========================================================================
-   */
+	/*
+	 * ===========================================================================
+	 * Erroneous template fragments.
+	 * ===========================================================================
+	 */
 
-  private $caseWithoutSwitch = <<<'TMPL'
+	private $caseWithoutSwitch = <<<'TMPL'
 <?php
 $msg = "This template contains a case statement without a switch statement";
 #| case 0
 TMPL;
 
-  private $undefinedSubstitutionValue = <<<'TMPL'
+	private $undefinedSubstitutionValue = <<<'TMPL'
 <?php
 $msg = "This template contains an undefined substitution values";
 /*# undefined */
 TMPL;
 
-  private $unclosedBlock = <<<'TMPL'
+	private $unclosedBlock = <<<'TMPL'
 <?php
 $msg = "This tempate contains an unclosed block";
 #{ if var = val
@@ -52,43 +48,43 @@ $msg = "This tempate contains an unclosed block";
 #}
 TMPL;
 
-  /*
-   * ===========================================================================
-   * Tests
-   * ===========================================================================
-   */
+	/*
+	 * ===========================================================================
+	 * Tests
+	 * ===========================================================================
+	 */
 
-  protected $parser;
+	protected $parser;
 
-  protected function setUp() {
-    $this->parser = new CodeTemplateParser();
-  }
+	protected function setUp() {
+		$this->parser = new CodeTemplateParser();
+	}
 
-  /**
-   * @expectedException zpt\pct\exception\ParseException
-   */
-  public function testCaseWithoutSwitch() {
-    $this->parser->parse($this->caseWithoutSwitch);
-  }
+	/**
+	 * @expectedException zpt\pct\exception\ParseException
+	 */
+	public function testCaseWithoutSwitch() {
+		$this->parser->parse($this->caseWithoutSwitch);
+	}
 
-  public function testUndefinedSubstitutionValue() {
-    $template = $this->parser->parse($this->undefinedSubstitutionValue);
+	public function testUndefinedSubstitutionValue() {
+		$template = $this->parser->parse($this->undefinedSubstitutionValue);
 
-    try {
-      $resolved = $template->forValues(array());
-      $this->fail("UndefinedValueException expected");
-    } catch (SubstitutionException $e) {
-      $previous = $e->getPrevious();
-      $this->assertInstanceOf('zpt\pct\UndefinedValueException', $previous);
-      $this->assertEquals('undefined', $previous->getVariableName());
-    }
-  }
+		try {
+			$resolved = $template->forValues(array());
+			$this->fail("UndefinedValueException expected");
+		} catch (SubstitutionException $e) {
+			$previous = $e->getPrevious();
+			$this->assertInstanceOf('zpt\pct\UndefinedValueException', $previous);
+			$this->assertEquals('undefined', $previous->getVariableName());
+		}
+	}
 
-  /**
-   * @expectedException zpt\pct\exception\ParseException
-   */
-  public function testUnclosedBlock() {
-    $this->parser->parse($this->unclosedBlock);
-  }
+	/**
+	 * @expectedException zpt\pct\exception\ParseException
+	 */
+	public function testUnclosedBlock() {
+		$this->parser->parse($this->unclosedBlock);
+	}
 
 }
